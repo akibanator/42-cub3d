5404cf2010d1e7e642c6f5c27d9c8b51c3d1b459
Ailton Kenji Akiba <ailtonkenji0@gmail.com>
2023-06-15 17:54:44 -0300

    feat: added some libraries on cub3d.h file

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: akenji-a <akenji-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 17:45:39 by akenji-a          #+#    #+#             */
-/*   Updated: 2023/06/15 17:49:06 by akenji-a         ###   ########.fr       */
+/*   Updated: 2023/06/15 17:53:23 by akenji-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@
 /********** INCLUDES **********/
 
 # include "libft.h"
+# include "unistd.h"
+# include "stdio.h"
+# include <stdlib.h>
+# include <errno.h>
+
 
 
 #endif
