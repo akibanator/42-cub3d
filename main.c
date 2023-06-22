7a78b2728efe9e06d5e80f143ab935a128f7d40f
Ailton Kenji Akiba <ailtonkenji0@gmail.com>
2023-06-22 16:41:45 -0300

    feat: added simple map layout for parser development

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 #include "cub3d.h"
 #include "mlx.h"
 
-int	main(void)
-{
-	void	*mlx;
-	void	*mlx_win;
+int main(void) {
+  void *mlx;
+  void *mlx_win;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 320, 240, "Cub3d");
-	(void)mlx_win;
-	mlx_loop(mlx);
-	return (0);
+  mlx = mlx_init();
+  mlx_win = mlx_new_window(mlx, 320, 240, "Cub3d");
+  (void)mlx_win;
+  mlx_loop(mlx);
+  return (0);
 }
